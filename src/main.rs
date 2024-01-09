a4ce9b3943106ebfae3727ea1f9607de3ef3e10a
Ximer <20438890+Ximer612@users.noreply.github.com>
2024-01-09 12:34:58 +0100

    Added player and camera spawn + imported textures, musics and sounds

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,54 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
-    .add_plugins(PeoplePlugin)
+    .add_systems(Startup, spawn_camera)
+    .add_systems(Startup, spawn_player)
     .run();
+}
+
+#[derive(Component)]
+pub struct Player {
+
+}
+
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>
+)
+{
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(
+        (
+            SpriteBundle{
+                transform: Transform::from_xyz(window.width() *0.5,window.height()*0.5,0.0),
+                texture: asset_server.load("sprites/mask.png"),
+                ..default()
+            },
+            Player {},
+        )
+    );
+
+}
+
+pub fn spawn_camera(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+)
+{
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(
+        Camera2dBundle{
+            transform: Transform::from_xyz(window.width() *0.5,window.height()*0.5,0.0),
+            ..default()
+    }
+);
+
 }
 
 pub struct PeoplePlugin;
